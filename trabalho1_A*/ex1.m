33b4888678705e068a734229bdf6b9e60a15bf12
sergio <srg.lcns@gmail.com>
2019-11-10 15:47:08 -0300

    att

--- trabalho1_A*/ex1.m
+++ trabalho1_A*/ex1.m
@@ -6,7 +6,6 @@
 % parte principal do exercício.
 % Este arquivo não deve ser alterado
 % author: raonifst at gmail dot com
-tic
 clear
 clc
 
@@ -18,7 +17,7 @@ id = intmin;
 
 % Teste 1
 M = [4 1 3; 9 2 5; 7 8 6];
-m = astar(M, @hamming);
+m = astar(M, @manhattan);
 % decomente esta linha para ver as movimentacoes
 reconstruct_path(m);
 assert(m.eq(Node([1 2 3; 4 5 6; 7 8 9], 0)));
@@ -26,29 +25,30 @@ fprintf('Teste 1: OK\n');
 
 % Teste 2
 M = [9 1 3; 4 2 5; 7 8 6];
-m = astar(M, @hamming);
+m = astar(M, @manhattan);
 reconstruct_path(m);
 assert(m.eq(Node([1 2 3; 4 5 6; 7 8 9], 0)));
 fprintf('Teste 2: OK\n');
 
 % Teste 3
 M = [4 9 5; 3 8 6; 7 1 2];
-m = astar(M, @hamming);
+m = astar(M, @manhattan);
 reconstruct_path(m);
 assert(m.eq(Node([1 2 3; 4 5 6; 7 8 9], 0)));
 fprintf('Teste 3: OK\n');
 
 % Teste 4
 M = [5 3 2; 7 6 4; 8 1 9];
-m = astar(M, @hamming);
+m = astar(M, @manhattan);
+reconstruct_path(m);
 assert(m.eq(Node([1 2 3; 4 5 6; 7 8 9], 0)));
 fprintf('Teste 4: OK\n');
 
 % Teste 5
 M = [4 6 7; 9 5 8; 2 1 3];
-m = astar(M, @hamming);
+m = astar(M, @manhattan);
+reconstruct_path(m);
 assert(m.eq(Node([1 2 3; 4 5 6; 7 8 9], 0)));
 fprintf('Teste 5: OK\n');
 
 fprintf('Sua implementação está correta\n');
-toc
