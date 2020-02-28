12ced7f73928427984113773fb0fc054ec5b6850
srg-lc <srg.lcns@gmail.com>
2020-02-27 20:27:59 -0400

    iniicio ap3

--- trabalho3/predict.m
+++ trabalho3/predict.m
@@ -1,7 +1,7 @@
 function p = predict(Theta1, Theta2, X)
 %PREDICT classica um conjunto de amostras utilizando os parâmetros da
 %rede (Theta1 e Theta2)
-% p = PREDICT(Theta1, Theta2, X) devolve um vetor com a classse de 
+% p = PREDICT(Theta1, Theta2, X) devolve um vetor com a classse de
 % cada amostra do conjunto X.
 
 % variaveis úteis
@@ -11,6 +11,14 @@ num_classes = size(Theta2, 1);
 % Voce deve preencher o seguinte vetor corretamente com a classe de cada
 % amostra
 p = zeros(m, 1);
+bias1 = ones(5000,1);
+a1 = [ones(size(X),1) X];
+z2 = a1*Theta1';
+a2_= [ones(size(z2),1) z2];
+a2 = sigmoid(a2_);
+z3 = a2*Theta2';
+a3 = sigmoid(z3);
+[a p] = max(a3);
 
 
 
