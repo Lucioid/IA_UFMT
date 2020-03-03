a5e564aec73bd74503a246a6529379d530fa88fb
srg-lc <srg.lcns@gmail.com>
2020-03-03 08:47:05 -0400

    Update cost_function.m

--- trabalho3/cost_function.m
+++ trabalho3/cost_function.m
@@ -5,7 +5,7 @@ function [J grad] = cost_function(nn_params, ...
                                    X, y, lambda)
 %COST_FUNCTION implementa a função de custo da rede neural
 %   [J grad] = COST_FUNCTON(nn_params, hidden_layer_size, num_labels, ...
-%   X, y, lambda) calcula a função de custo  e o gradiente da rede. 
+%   X, y, lambda) calcula a função de custo  e o gradiente da rede.
 %
 
 % Não altere!!
@@ -33,7 +33,18 @@ for i=1:m
 end
 
 % =============== Sua implementação deve ser vir aqui ==================
+%Obter valores da ultima camada por feedforward
+p = zeros(m, 1);
+bias1 = ones(5000,1);
+a1 = [ones(size(X),1) X];
+z2 = a1*Theta1';
+a2_= [ones(size(z2),1) z2];
+a2 = sigmoid(a2_);
+z3 = a2*Theta2';
+a3 = sigmoid(z3);
 
+%erro da camada de saída
+E1 = (a3-y)
 
 
 
